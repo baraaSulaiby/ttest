4abe177560cc77888f75b8365027a95945ee70c4
baraaSulaiby <85201848+baraaSulaiby@users.noreply.github.com>
2022-04-10 02:52:07 +0300

    Add files via upload
    userStory1

---
+++ AboutViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace homepage.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+    
+        }
+
+        public ICommand OpenWebCommand { get; }
+
+    }
+}
---
+++ TestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homepage.ViewModels
+{
+    internal class TestViewModel
+    {
+    }
+}
